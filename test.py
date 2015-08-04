0d1da23bc263f8c8b103c90572e3dfe532da9ddc
jajcayn <jajcay@cs.cas.cz>
2015-08-04 16:49:29 +0200

    test

---
+++ test.py
@@ -0,0 +1,81 @@
+from enso_CMImap import load_enso_SSTs
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from matplotlib.ticker import MultipleLocator, FuncFormatter
+import cPickle
+import sys
+import matplotlib.gridspec as gridspec
+sys.path.append('/home/nikola/Work/phd/multi-scale/src')
+sys.path.append('/home/nikola/Work/phd/multi-scale/surrogates')
+sys.path.append('/home/nikola/Work/phd/mutual_information')
+
+from data_class import DataField
+from surrogates import SurrogateField
+from multiprocessing import Process, Queue
+
+CMIP5model = None
+
+enso_raw = np.loadtxt("nino34m13.txt") # length x 2 as 1st column is continuous year, second is SST in degC
+enso = DataField()
+
+enso.data = enso_raw[:, 1]
+
+time = np.zeros_like(enso.data, dtype = np.int32)
+y = np.int(enso_raw[0, 0])
+start_date = date(y, 1, 1)
+delta = relativedelta(months = +1)
+d = start_date
+for i in range(time.shape[0]):
+    time[i] = d.toordinal()
+    d += delta
+
+enso.time = time
+enso.location = 'NINO3.4 SSTs'
+
+a = list(enso.get_seasonality(DETREND = False))
+enso_sg = SurrogateField()
+
+_, _, idx = enso.get_data_of_precise_length(length = 1024, end_date = date(2014, 1, 1), COPY = False)
+enso_sg.copy_field(enso)
+enso_sg.data = enso_sg.data[idx[0]:idx[1]]
+
+enso.return_seasonality(a[0], a[1], None)
+
+a[0] = a[0][idx[0]:idx[1]]
+a[1] = a[1][idx[0]:idx[1]]
+enso.get_data_of_precise_length(length = 1024, end_date = date(2014, 1, 1), COPY = True)
+
+enso.center_data()
+print enso.data[:20]
+
+def _surrs(sg, a, jobq, resq):
+    mean, var, _ = a
+    while jobq.get() is not None:
+        sg.construct_fourier_surrogates_spatial()
+        sg.add_seasonality(mean, var, None)
+
+        sg.center_surr()
+        
+        resq.put(sg.get_surr())
+
+numsurr = 100
+comp = 0
+jobq = Queue()
+resq = Queue()
+for i in range(numsurr):
+    jobq.put(1)
+for i in range(3):
+    jobq.put(None)
+wrkrs = [Process(target=_surrs, args = (enso_sg, a, jobq, resq)) for i in range(3)]
+for w in wrkrs:
+    w.start()
+    
+while comp < numsurr:
+    ts = resq.get()
+    print comp, ts[:20]
+    comp += 1
+    
+for w in wrkrs:
+    w.join()
